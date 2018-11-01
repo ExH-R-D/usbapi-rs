91587cae855be4fed98cbb60533ac108fa4c1e8e
Mikael Hermansson <mikael.hermansson@mykolab.com>
2018-11-02 00:09:14 +0100

    Descriptors almost done but needs some refactoring

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,17 @@ use std::fmt;
 struct LinuxUsbDevices {
 }
 
+enum DescriptorType {
+    Device = 1,
+    Configuration = 2,
+    UTFString = 3,
+    Interface = 4,
+    Endpoint = 5,
+    Class_Specific = 0x24,
+    Hub = 0x29,
+    SS_Endpoint_Companion = 0x30
+}
+
 struct DeviceDescriptor {
     length: u8,
     descriptor_type: u8,
@@ -37,6 +48,29 @@ struct ConfigurationDescriptor {
     iconfiguration: u8,
     bmattributes: u8,
     max_power: u8,
+    pub interfaces: Vec<InterfaceDescriptor>
+}
+
+struct InterfaceDescriptor {
+    length: u8,
+    descriptor_type: u8,
+    interface_number: u8,
+    alternate_setting: u8,
+    num_endpoints: u8,
+    interface_class: u8,
+    interface_sub_class:u8,
+    interface_protocol: u8,
+    iinterface: u8,
+    pub endpoints: Vec<EndpointDescriptor>
+}
+
+struct EndpointDescriptor {
+    length: u8,
+    descriptor_type: u8,
+    endpoint_address: u8,
+    bm_attributes: u8,
+    max_packet_size: u16,
+    interval: u8
 }
 
 impl fmt::Display for DeviceDescriptor {
@@ -72,44 +106,109 @@ impl fmt::Display for ConfigurationDescriptor {
         d+=&format!("iConfiguration: {}\n", self.iconfiguration);
         d+=&format!("bmAttributes: 0x{:02x}\n", self.bmattributes);
         d+=&format!("bMaxPower: {}\n", self.max_power);
+        for iface in &self.interfaces {
+            d+=&format!("{}", iface);
+        }
+        write!(f, "{}", d)
+    }
+}
+
+impl fmt::Display for InterfaceDescriptor {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut d = format!("bLength: {}\n", self.length);
+        d+=&format!("bDescriptorType: {}\n", self.descriptor_type);
+        d+=&format!("bInterfaceNumber: {}\n", self.interface_number);
+        d+=&format!("bAlternateSetting: {}\n", self.alternate_setting);
+        d+=&format!("bNumEndpoints: {}\n", self.num_endpoints);
+        d+=&format!("bInterfaceClass: {}\n", self.interface_class);
+        d+=&format!("bInterfaceSubClass: {}\n", self.interface_sub_class);
+        d+=&format!("bInterfaceProtocol: {}\n", self.interface_protocol);
+        d+=&format!("bInterfaceNumber: {}\n", self.interface_number);
+        d+=&format!("iInterface: {}\n", self.iinterface);
+        for endpoint in &self.endpoints {
+            d+=&format!("{}", endpoint);
+        }
+        write!(f, "{}", d)
+    }
+}
+
+impl fmt::Display for EndpointDescriptor {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut d = format!("bLength: {}\n", self.length);
+        d+=&format!("bDescriptorType: {}\n", self.descriptor_type);
+        d+=&format!("bEndpointAddress: 0x{:02X}\n", self.endpoint_address);
+        d+=&format!("bmAttributes: {}\n", self.bm_attributes);
+        d+=&format!("wMaxPacketSize: {}\n", self.max_packet_size);
+        d+=&format!("bInterval: {}\n", self.interval);
         write!(f, "{}", d)
     }
 }
 
 impl DeviceDescriptor {
-    fn new(iter: &mut Iter<u8>) -> Self {
-        DeviceDescriptor {
-            length: (iter.len() + 2) as u8,
-            descriptor_type: 1,
-            bcd_usb: *iter.next().unwrap_or(&0) as u16 | (*iter.next().unwrap_or(&0) as u16) << 8,
-            device_class: *iter.next().unwrap_or(&0),
-            device_sub_class: *iter.next().unwrap_or(&0),
-            device_protocol: *iter.next().unwrap_or(&0),
-            max_packet_size0: *iter.next().unwrap_or(&0),
-            id_vendor: *iter.next().unwrap_or(&0) as u16 | (*iter.next().unwrap_or(&0) as u16) << 8,
-            id_product: (*iter.next().unwrap_or(&0) as u16) | (*iter.next().unwrap_or(&0) as u16) << 8,
-            bcd_device: *iter.next().unwrap_or(&0) as u16 | (*iter.next().unwrap_or(&0) as u16) << 8,
-            imanufacturer: *iter.next().unwrap_or(&0),
-            iproduct: *iter.next().unwrap_or(&0),
-            iserial_number: *iter.next().unwrap_or(&0),
-            num_configurations: *iter.next().unwrap_or(&0),
+    fn new(iter: &mut Iter<u8>) -> Option<Self> {
+        Some(DeviceDescriptor {
+            length: *iter.next()?,
+            descriptor_type: *iter.next()?,
+            bcd_usb: *iter.next()? as u16 | (*iter.next().unwrap_or(&0) as u16) << 8,
+            device_class: *iter.next()?,
+            device_sub_class: *iter.next()?,
+            device_protocol: *iter.next()?,
+            max_packet_size0: *iter.next()?,
+            id_vendor: *iter.next()? as u16 | (*iter.next()? as u16) << 8,
+            id_product: (*iter.next()? as u16) | (*iter.next()? as u16) << 8,
+            bcd_device: *iter.next()? as u16 | (*iter.next()? as u16) << 8,
+            imanufacturer: *iter.next()?,
+            iproduct: *iter.next()?,
+            iserial_number: *iter.next()?,
+            num_configurations: *iter.next()?,
             configurations: vec![]
-        }
+        })
     }
 }
 
 impl ConfigurationDescriptor {
-    fn new(iter: &mut Iter<u8>) -> Self {
-        ConfigurationDescriptor {
-            length: iter.len() as u8,
-            descriptor_type: 2,
-            total_length: *iter.next().unwrap_or(&0) as u16 | (*iter.next().unwrap_or(&0) as u16) << 8,
-            num_interfaces: *iter.next().unwrap_or(&0),
-            configuration_value: *iter.next().unwrap_or(&0),
-            iconfiguration: *iter.next().unwrap_or(&0),
-            bmattributes: *iter.next().unwrap_or(&0),
-            max_power: *iter.next().unwrap_or(&0)
-        }
+    fn new(iter: &mut Iter<u8>) -> Option<Self> {
+        Some(ConfigurationDescriptor {
+            length: *iter.next()?,
+            descriptor_type: *iter.next()?,
+            total_length: *iter.next()? as u16 | (*iter.next()? as u16) << 8,
+            num_interfaces: *iter.next()?,
+            configuration_value: *iter.next()?,
+            iconfiguration: *iter.next()?,
+            bmattributes: *iter.next()?,
+            max_power: *iter.next()?,
+            interfaces: vec![]
+        })
+    }
+}
+
+impl InterfaceDescriptor {
+    fn new(iter: &mut Iter<u8>) -> Option<Self> {
+        Some(InterfaceDescriptor {
+            length: *iter.next()?,
+            descriptor_type: *iter.next()?,
+            interface_number: *iter.next()?,
+            alternate_setting: *iter.next()?,
+            num_endpoints: *iter.next()?,
+            interface_class: *iter.next()?,
+            interface_sub_class: *iter.next()?,
+            interface_protocol: *iter.next()?,
+            iinterface: *iter.next()?,
+            endpoints: vec![]
+        })
+    }
+}
+
+impl EndpointDescriptor {
+    fn new(iter: &mut Iter<u8>) -> Option<Self> {
+        Some(EndpointDescriptor {
+            length: *iter.next()?,
+            descriptor_type: *iter.next()?,
+            endpoint_address: *iter.next()?,
+            bm_attributes: *iter.next()?,
+            max_packet_size: *iter.next()? as u16 | (*iter.next()? as u16) << 8,
+            interval: *iter.next()?
+        })
     }
 }
 
@@ -152,24 +251,63 @@ impl LinuxUsbDevices {
         let mut iter = data.iter();
         let dlength = *iter.next().unwrap_or(&0) as usize;
         let typ = *iter.next().unwrap_or(&0);
-        // This feels awkward should use take(dlength) or similar here...
-        let mut iter_desc = data[2..dlength].iter();
-        let mut desc = DeviceDescriptor::new(&mut iter_desc);
+        let mut iter_desc = data[..dlength].iter();
+        let mut desc = DeviceDescriptor::new(&mut iter_desc).expect("Could not parse desc");
         let mut confs = desc.num_configurations;
-        let mut start = dlength;
+        let mut data = &data[dlength..];
         while confs > 0 {
             confs-= 1;
-            let mut iter = data[start..start+2].iter();
-            start+=2;
+            let mut iter = data[..2].iter();
             let dlength = *iter.next().unwrap_or(&0) as usize;
             let typ = *iter.next().unwrap_or(&0);
-            let mut iter_desc = data[start..start+dlength].iter();
-            start+=dlength;
-            let conf = ConfigurationDescriptor::new(&mut iter_desc);
+            let mut iter_desc = data[..dlength].iter();
+            let mut conf = ConfigurationDescriptor::new(&mut iter_desc).expect("Could not parse config desciprtiion");
+            data = &data[dlength..];
+            let mut numifaces = conf.num_interfaces;
+            while numifaces > 0 {
+                let mut iter = data[..2].iter();
+                let mut dlength = *iter.next().unwrap_or(&0) as usize;
+                let typ = *iter.next().unwrap_or(&0);
+                let mut iter_desc = data[..dlength].iter();
+                // FIXME enum and redo this madness
+                match typ {
+                    4 => match InterfaceDescriptor::new(&mut iter_desc) {
+                        Some(mut iface) => {
+                            numifaces-=1;
+                            let mut epoints = iface.num_endpoints;
+                            while epoints > 0 {
+                                data = &data[dlength..];
+                                let mut iter = data[..2].iter();
+                                dlength = *iter.next().unwrap_or(&0) as usize;
+                                let typ = *iter.next().unwrap_or(&0);
+                                let mut iter_desc = data[..dlength].iter();
+                                match EndpointDescriptor::new(&mut iter_desc) {
+                                    Some(endpoint) => {
+                                        iface.endpoints.push(endpoint);
+                                        epoints-=1;
+                                    },
+                                    None => {
+                                        eprintln!("{}:{} Could not parser EndpointDescriptor for {:02X?}", bus, address, &data[..dlength]);
+                                    }
+                                }
+                            }
+                            conf.interfaces.push(iface);
+                        },
+                        None => {
+                            eprintln!("{}:{} Could not parser InterfaceDescriptor for {:?}", bus, address, &data[..dlength]);
+                        }
+                    },
+                    typ => eprintln!("Uknown descriptor {:02X}", typ),
+                }
+                data = &data[dlength..];
+            }
             desc.configurations.push(conf);
         }
-
         println!("{}:{}\n{}", bus, address, desc);
+
+        if data.len() > 0 {
+            println!("rest: {:02X?}", data);
+        }
     }
 
 }
