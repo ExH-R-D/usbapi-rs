1c1b1f757e2d3827ecf36c2aedd3337234530ba6
Mikael Hermansson <mikael.hermansson@mykolab.com>
2018-11-22 01:10:58 +0100

    poll test using mio

--- examples/stm32.rs
+++ examples/stm32.rs
@@ -1,13 +1,13 @@
+extern crate mio;
 extern crate usbapi;
-use usbapi::os::linux::usbfs::UsbFs;
 use usbapi::os::linux::enumerate::Enumerate;
-
+use usbapi::os::linux::usbfs::UsbFs;
+use mio::{Events,Ready, Poll, PollOpt, Token, Evented};
+use std::path::Path;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
-use std::path::Path as Path;
-use std::time::Duration;
 use std::thread;
-
+use std::time::Duration;
 fn main() {
     let term = Arc::new(AtomicBool::new(false));
     signal_hook::flag::register(signal_hook::SIGQUIT, Arc::clone(&term)).unwrap();
@@ -20,11 +20,15 @@ fn main() {
     for device in usb.devices() {
         if device.device.id_vendor == 0x483 && device.device.id_product == 0x5740 {
             let mut usb = UsbFs::from_device(&device).expect("FIXME actually cant fail");
-            println!("Capabilities: 0x{:02X?}", usb.capabilities());
+            let poll = Poll::new().unwrap();
+            usb.register(&poll,
+             Token(0), Ready::all(), PollOpt::edge());
+
+             println!("Capabilities: 0x{:02X?}", usb.capabilities());
             usb.claim_interface(0).is_ok();
             usb.claim_interface(1).is_ok();
             match usb.control() {
-                Ok(_) => {},
+                Ok(_) => {}
                 Err(err) => println!("Send bytes to control failed {}", err),
             };
 
@@ -36,20 +40,30 @@ fn main() {
             let len = usb.bulk_write(1, "$".to_string().as_bytes()).unwrap_or(0);
             println!("{} sent data", len);
             let len = usb.bulk_read(1, &mut mem).unwrap_or(0);
-            println!("{} data: {:?}", len, &mem[0..len as usize]);
-            println!("As string: {}", String::from_utf8_lossy(&mem[0..len as usize]));
+            println!(
+                "As string: {}",
+                String::from_utf8_lossy(&mem[0..len as usize])
+            );
 
             let mut s: [u8; 1] = ['$' as u8; 1];
             let len = usb.async_transfer(0x1, &mut s).unwrap_or(0);
             println!("{} sent data", len);
-            let len = usb.bulk_read(1, &mut mem).unwrap_or(1);
- 
+            let mut events = Events::with_capacity(16);
             loop {
+                poll.poll(&mut events, Some(Duration::from_millis(100)));
+                for e in &events {
+                    println!("event: {:?}", e);
+                    let len = usb.bulk_read(1, &mut mem).unwrap_or(1);
+                    println!(
+                        "As string: {}",
+                        String::from_utf8_lossy(&mem[0..len as usize])
+                    );
+                }
                 // TODO setup a thread to talk to STM via http
                 if term.load(Ordering::Relaxed) {
                     break;
                 }
-                thread::sleep(Duration::from_millis(100));
+//                thread::sleep(Duration::from_millis(100));
             }
             break;
         }
--- src/os/linux/usbfs.rs
+++ src/os/linux/usbfs.rs
@@ -4,6 +4,36 @@ use std::ffi::CString;
 use std::fs::OpenOptions;
 use std::mem;
 use std::os::unix::io::AsRawFd;
+use mio::{Ready, Poll, PollOpt, Token};
+use mio::event::Evented;
+use mio::unix::EventedFd;
+use std::io;
+impl Evented for UsbFs {
+    fn register(
+        &self,
+        poll: &Poll,
+        token: Token,
+        interest: Ready,
+        opts: PollOpt,
+    ) -> io::Result<()> {
+        EventedFd(&self.handle.as_raw_fd()).register(poll, token, interest, opts)
+    }
+
+    fn reregister(
+        &self,
+        poll: &Poll,
+        token: Token,
+        interest: Ready,
+        opts: PollOpt,
+    ) -> io::Result<()> {
+        EventedFd(&self.handle.as_raw_fd()).reregister(poll, token, interest, opts)
+    }
+
+    fn deregister(&self, poll: &Poll) -> io::Result<()> {
+        EventedFd(&self.handle.as_raw_fd()).deregister(poll)
+    }
+}
+
 
 #[macro_export]
 macro_rules! ioctl_read_ptr {
