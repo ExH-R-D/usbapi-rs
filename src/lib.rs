929bf375d75b7961b9bbf9176adc88777c5d3f6b
Mikael Hermansson <mikael.hermansson@mykolab.com>
2019-01-18 21:08:58 +0100

    yeha new release of rust no need for ridicilous crate:: at begining of import line

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
-#[macro_use]
 use nix::*;
 pub mod descriptors;
 pub mod os;
-pub use crate::os::linux::usbfs::{UsbCoreTransfer, UsbTransfer};
+pub use os::linux::usbfs::{UsbCoreTransfer, UsbTransfer};
 /* Unsure if this is the correct way do it... */
-#[cfg(target_os="linux")] pub use crate::os::linux::enumerate::UsbEnumerate;
-#[cfg(target_os="linux")] pub use crate::os::linux::usbfs::UsbFs as UsbCore;
-#[cfg(target_os="linux")] pub use crate::os::linux::usbfs::ControlTransfer;
+#[cfg(target_os="linux")] pub use os::linux::enumerate::UsbEnumerate;
+#[cfg(target_os="linux")] pub use os::linux::usbfs::UsbFs as UsbCore;
+#[cfg(target_os="linux")] pub use os::linux::usbfs::ControlTransfer;
