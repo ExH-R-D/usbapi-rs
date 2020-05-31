28794be6220eb0d164ab7567733ba996962b458c
Mikael <mike7b4@protonmail.com>
2020-05-31 17:14:27 +0200

    split usb_device in separate module

---
+++ src/os/linux/usb_device.rs
@@ -0,0 +1,96 @@
+use crate::descriptors::descriptor::{Descriptor, DescriptorType};
+use crate::descriptors::device::Device;
+use crate::UsbCore;
+use serde::{Deserialize, Serialize};
+use std::fmt;
+use std::io::prelude::*;
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UsbDevice {
+    pub bus: u8,
+    // FIXME change name to device
+    pub address: u8,
+    pub manufacturer: String,
+    pub product: String,
+    pub serial: String,
+    pub device: Device,
+}
+
+impl fmt::Display for UsbDevice {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}:{}\n{}", self.bus, self.address, self.device)
+    }
+}
+
+impl UsbDevice {
+    pub(crate) fn new(
+        bus: u8,
+        dev: u8,
+        device: Device,
+        manufacturer: String,
+        product: String,
+        serial: String,
+    ) -> Self {
+        UsbDevice {
+            bus,
+            address: dev,
+            device,
+            product,
+            manufacturer,
+            serial,
+        }
+    }
+
+    pub(crate) fn from_usb_raw(usb: &mut UsbCore) -> Option<Self> {
+        let device;
+        let descs = if let Ok(mut descs) = Descriptor::from_bytes(usb.handle().bytes()) {
+            if let DescriptorType::Device(dev) = descs
+                .next()
+                .unwrap_or_else(|| DescriptorType::Unknown(vec![]))
+            {
+                let manufacturer = usb.get_descriptor_string(dev.imanufacturer.clone());
+                let product = usb.get_descriptor_string(dev.iproduct.clone());
+                let serial = usb.get_descriptor_string(dev.iserial.clone());
+                device = UsbDevice::new(
+                    usb.bus_dev.0,
+                    usb.bus_dev.1,
+                    dev,
+                    manufacturer,
+                    product,
+                    serial,
+                );
+                Some((descs, device))
+            } else {
+                log::error!("Descriptor read failed");
+                None
+            }
+        } else {
+            return None;
+        };
+
+        let (descs, mut device) = descs?;
+        for kind in descs {
+            match kind {
+                DescriptorType::Configuration(conf) => {
+                    device.device.configurations.push(conf);
+                }
+                DescriptorType::Interface(iface) => {
+                    if let Some(c) = device.device.configurations.last_mut() {
+                        c.interfaces.push(iface);
+                    }
+                }
+                DescriptorType::String(_) => {}
+                DescriptorType::Endpoint(endpoint) => {
+                    if let Some(c) = device.device.configurations.last_mut() {
+                        if let Some(i) = c.interfaces.last_mut() {
+                            i.endpoints.push(endpoint);
+                        }
+                    }
+                }
+                _ => {
+                    log::warn!("Unknown descriptor type: {:?}", kind);
+                }
+            };
+        }
+        Some(device)
+    }
+}
