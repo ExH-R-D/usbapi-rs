f732ced42d65ead81c07b44a6e94b2887f7e759c
Mike7b4 <mike7b4@protonmail.com>
2021-04-07 11:18:18 +0000

    Asynchanges

--- examples/stm32.rs
+++ examples/stm32.rs
@@ -1,9 +1,13 @@
+use mio::{Events, Interest, Poll, Token};
 /// This example is ugly
 /// For more see: https://gitlab.com/mike7b4/dfuflash
-use mio::{Events, Interest, Poll, Token};
+#[cfg(feature = "mio")]
+use std::io::ErrorKind;
+use std::io::Write;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
-use std::time::Duration;
+use std::time::{Duration, Instant};
+use usbapi::endpoint::Endpoint;
 use usbapi::*;
 const SYNC_BYTE: u8 = 250;
 const BULK_IN: u8 = 0x81;
@@ -11,23 +15,24 @@ const BULK_OUT: u8 = 0x1;
 
 fn block_transfer(usb: &mut UsbCore) -> Result<(), std::io::Error> {
     let mut mem: [u8; 64] = [0; 64];
-    let len = usb.bulk_read(1, &mut mem, 1.into()).unwrap_or(0);
+    let len = usb.bulk_write(1, &[SYNC_BYTE], 1.into()).unwrap_or(0);
+    let len = 0;
     println!("Read once to check if there where some garbage");
     println!("1 {} received data {:?}", len, &mem[0..len as usize]);
+    let mut mem: [u8; 64] = [0; 64];
     let len = usb
         .bulk_read(1, &mut mem, TimeoutMillis::from(1))
         .unwrap_or(0);
     println!("2 {} received data: {:?}", len, &mem[0..len as usize]);
-    assert!(len == 0);
     let len = usb
         .bulk_write(1, "$".to_string().as_bytes(), 1.into())
         .unwrap_or(0);
-    assert!(len == 1);
     println!("1 {} sent data", len);
+    let mut mem: [u8; 64] = [0; 64];
     let len = usb
         .bulk_read(1, &mut mem, TimeoutMillis::from(1))
         .unwrap_or(0);
-    assert!(len > 0);
+    //assert!(len > 0);
     println!(
         "3 Received data try stringify: {}",
         String::from_utf8_lossy(&mem[0..len as usize])
@@ -35,42 +40,91 @@ fn block_transfer(usb: &mut UsbCore) -> Result<(), std::io::Error> {
     Ok(())
 }
 
-fn poll_send(poll: &mut Poll, usb: &mut UsbCore) -> Result<(), std::io::Error> {
-    let mut events = Events::with_capacity(1);
-    let mut urbtx = usb.new_bulk(BULK_OUT, 1)?;
-    let slice = urbtx.buffer_from_raw_mut();
-    slice[0] = SYNC_BYTE;
-    println!("=== urbtx before poll ====\n{}", urbtx);
-    usb.async_transfer(urbtx)?;
-    poll.poll(&mut events, Some(Duration::from_millis(100)))
-        .expect("Poll bailed");
-    if events.is_empty() {
-        panic!("Poll did not return anything");
+/// Handle async response
+fn handle_response(
+    mut transfer: TransferKind,
+    bulk_tx: &mut Option<BulkTransfer>,
+    bulk_rx: &mut Option<BulkTransfer>,
+    send_again: &mut bool,
+) {
+    match transfer {
+        TransferKind::Control(mut control) => {
+            log::info!("{}", control);
+            control.flush();
+        }
+        TransferKind::Bulk(mut bulk) => {
+            if bulk.endpoint.is_bulk_out() {
+                assert!(bulk.actual_length == bulk.buffer_length);
+                *bulk_tx = Some(bulk);
+            } else {
+                assert!(bulk.endpoint == Endpoint::new(0x81));
+                let raw = bulk.buffer_from_raw();
+                let s: Vec<u8> = raw.iter().cloned().filter(|c| *c < 127).collect();
+                print!("{}", String::from_utf8_lossy(s.as_slice()).to_string());
+                if raw[0] == SYNC_BYTE
+                    || s.ends_with(&[0x0A, 0x2E])
+                    || s.ends_with(&[0x0A, 0x2C])
+                    || (raw[0] == 0x2E && s.len() == 1)
+                    || (raw[0] == 0x2C && s.len() == 1)
+                {
+                    *send_again = true;
+                }
+                bulk.flush();
+                *bulk_rx = Some(bulk);
+            }
+        }
+        TransferKind::Invalid(ep) => {
+            log::error!("Invalid endpoint {}", ep);
+        }
     }
-    urbtx = usb.async_response()?;
-    println!("=== urb tx after poll ===\n{}", urbtx);
-    Ok(())
 }
 
+#[cfg(feature = "mio")]
 fn poll_transfer(terminate: Arc<AtomicBool>, usb: &mut UsbCore) -> Result<(), std::io::Error> {
     let mut poll = Poll::new().unwrap();
-    let mut events = Events::with_capacity(1);
+    let mut events = Events::with_capacity(64);
     poll.registry()
         .register(usb, Token(0), Interest::WRITABLE)?;
 
-    poll_send(&mut poll, usb)?;
-    // send one byte
-    let mut urbrx = usb.new_bulk(BULK_IN, 64)?;
-    usb.async_transfer(urbrx).unwrap_or(0);
-    while !terminate.load(Ordering::Relaxed) {
-        poll.poll(&mut events, Some(Duration::from_millis(100)))?;
-        for e in &events {
-            println!("got event: {:?}", e);
-            urbrx = usb.async_response().unwrap();
-            println!("=== urb rx after poll ===\n{}", urbrx);
-            println!("Got bytes:\n{:02X?}", urbrx.buffer_from_raw());
-            //poll_send(&poll, usb);
-            usb.async_transfer(urbrx).unwrap_or(0);
+    let mut bulk_rx = Some(usb.new_bulk_in(1, 64)?);
+    let mut bulk_tx = Some(usb.new_bulk_out(1, 64)?);
+    if let Some(bulk_rx) = bulk_rx.take() {
+        usb.submit_bulk(bulk_rx)?;
+    }
+    if let Some(mut bulk_tx) = bulk_tx.take() {
+        bulk_tx.write_all(&[SYNC_BYTE, 0xC8])?;
+        // submit what we wrote
+        usb.submit_bulk(bulk_tx)?;
+    }
+    let mut send_again = false;
+    let mut every = 0;
+    let end = Instant::now();
+    while !terminate.load(Ordering::Relaxed) && end.elapsed() < Duration::from_secs(10) {
+        if let Some(bulk_rx) = bulk_rx.take() {
+            usb.submit_bulk(bulk_rx)?;
+        }
+
+        let instant = Instant::now();
+        poll.poll(&mut events, Some(Duration::from_millis(10)))?;
+        if !events.is_empty() && (every % 10) == 0 {
+            println!("{:?}", instant.elapsed());
+        }
+        every += 1;
+        if events.is_empty() {
+            if send_again {
+                if let Some(mut bulk_tx) = bulk_tx.take() {
+                    send_again = false;
+                    bulk_tx.flush();
+                    bulk_tx.write_all(&[0x3F, 0x0A])?;
+                    usb.submit_bulk(bulk_tx)?;
+                }
+            }
+        }
+        for _ in &events {
+            usb.async_response_all()?;
+            for transfer in usb.collect_responses() {
+                handle_response(transfer, &mut bulk_tx, &mut bulk_rx, &mut send_again);
+            }
         }
     }
     Ok(())
@@ -81,6 +135,7 @@ fn main() -> Result<(), std::io::Error> {
     signal_hook::flag::register(signal_hook::SIGQUIT, Arc::clone(&term)).unwrap();
     signal_hook::flag::register(signal_hook::SIGTERM, Arc::clone(&term)).unwrap();
     signal_hook::flag::register(signal_hook::SIGINT, Arc::clone(&term)).unwrap();
+    simple_logger::SimpleLogger::new().with_level(log::LevelFilter::Info);
 
     let usb = UsbEnumerate::from_sysfs()?;
 
@@ -106,24 +161,20 @@ fn main() -> Result<(), std::io::Error> {
                     .unwrap_or("?".into())
             );
             let _ = usb.claim_interface(1).is_ok();
-            match usb.control_async_wait(ControlTransfer::new_nodata(
-                0x21,
-                0x22,
-                0x3,
-                0,
-                TimeoutMillis::from(100),
-            )) {
-                Ok(_) => {}
+            let ctrl = usb.new_control_nodata(0x21, 0x22, 0x3, 0)?;
+            match usb.control_async_wait(ctrl, TimeoutMillis::from(100)) {
+                Ok(control) => println!("{}", control),
                 Err(err) => println!("Send bytes to control failed {}", err),
             };
 
             block_transfer(&mut usb)?;
 
+            #[cfg(feature = "mio")]
             poll_transfer(term.clone(), &mut usb)?;
             break;
         }
     }
 
-    println!("Exited succefully");
+    println!("Exited successfully");
     Ok(())
 }
